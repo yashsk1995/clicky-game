289657928e17773c73cd3ffdd57ec44e95d32fb4
yashK <yashkinariwalask95@gmail.com>
2018-09-26 11:07:48 -0400

    suffle array done!

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -12,17 +12,33 @@ class App extends React.Component{
         topscore:0,
         clickedimg:[]
     };
+    shuffleArray(array) {
+        for (var i = array.length - 1; i > 0; i--) {
+            var j = Math.floor(Math.random() * (i + 1));
+            var temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
 
     clicked = id =>{
         let array  =  this.state.clickedimg;
         if(array.includes(id)){
             this.setState({score :0});
-            this.setState({topscore : 0});
             this.setState({clickedimg : []});
             console.log("Game OVer!!");
         }
         else{
+        // let temp=0;
         array.push(id);
+        this.shuffleArray(this.state.cards);
+        this.setState({score: this.state.score+1});
+            
+        if(this.state.score >= this.state.topscore){
+            
+            this.setState({topscore:this.state.score});
+            }
+
         console.log(array);
     }
 }
